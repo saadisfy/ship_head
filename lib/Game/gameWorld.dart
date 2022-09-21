0a3b0897983aa15ad29bfc97a9c0c7f43772be87
Saad <saadmasood@web.de>
2022-09-21 03:21:18 +0200

    create map and a moveable player and joystick

---
+++ lib/Game/gameWorld.dart
@@ -0,0 +1,95 @@
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+import 'package:flame/extensions.dart';
+import 'package:flame/game.dart';
+import 'package:flame/gestures.dart';
+import 'package:flame/sprite.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:ship_head/Game/player.dart';
+
+class GameWorld extends BaseGame with PanDetector {
+  late Player player;
+  Offset? _pointerStartPosition;
+  Offset? _pointerCurrentPosition;
+
+  final double joyStickRadius = 60;
+  final double _joyStickInnerRadius = 20;
+  @override
+  Future<void> onLoad() async {
+    final spreadsheetFileName = "simpleSpace_sheet@2.png";
+    await images.load(spreadsheetFileName);
+    final spriteSheet = SpriteSheet.fromColumnsAndRows(
+        image: images.fromCache(spreadsheetFileName), columns: 8, rows: 6
+    );
+    player = Player(
+      sprite: spriteSheet.getSpriteById(4),
+      size: Vector2(64, 64),
+      position: viewport.canvasSize / 2,
+    );
+    player.anchor = Anchor.center;
+
+    add(player);
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+
+
+    //draw Joystick outer circle
+    if (_pointerStartPosition != null) {
+      //user touching the screen
+      canvas.drawCircle(
+          _pointerStartPosition!,
+          joyStickRadius,
+          Paint()..color= Colors.grey.withAlpha(100)
+      );
+    }
+    //draw joystick inner circle
+    if (_pointerCurrentPosition != null && _pointerStartPosition!= null) {
+      var delta =  _pointerCurrentPosition! - _pointerStartPosition!;
+      if (delta.distance > joyStickRadius) {
+        delta = (_pointerStartPosition! +
+            (Vector2(delta.dx, delta.dy).normalized() * joyStickRadius).toOffset());
+      } else {
+        delta = _pointerCurrentPosition!;
+      }
+      canvas.drawCircle(
+          delta,
+          _joyStickInnerRadius,
+          Paint()..color= Colors.white.withAlpha(100)
+      );
+    }
+  }
+
+  @override
+  void onPanStart(DragStartDetails details) {
+    _pointerStartPosition = details.globalPosition;
+    _pointerCurrentPosition = details.globalPosition;
+  }
+  @override
+  void onPanUpdate(DragUpdateDetails details) {
+    _pointerCurrentPosition = details.globalPosition;
+    var delta = _pointerCurrentPosition! - _pointerStartPosition!;
+    if (delta.distance > 10 ) {
+      //deadzone of the joystick
+      player.setMoveDirection(Vector2(delta.dx, delta.dy));
+    } else {
+      player.setMoveDirection(Vector2.zero());
+    }
+  }
+  @override
+  void onPanEnd(DragEndDetails details) {
+    _pointerStartPosition = null;
+    _pointerCurrentPosition = null;
+    player.setMoveDirection(Vector2.zero());// after letting go set direction to 0
+  }
+  @override
+  void onPanCancel() {
+    _pointerStartPosition = null;
+    _pointerCurrentPosition = null;
+    player.setMoveDirection(Vector2.zero());
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter/widgets.dart';
+import 'package:ship_head/Game/gameWorld.dart';
+
+void main() {
+  runApp(GameWidget(game: GameWorld()));
+}
